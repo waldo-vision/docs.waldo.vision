224ac9a2519e3c0d97a821a36ae90aba6577f3a7
tsundere <benlmartin.work@gmail.com>
2021-11-29 09:03:31 +0000

    Adding the edit link for ease for adding new documentation and editing already existing documents

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/waldo-vision/docs',
+          editUrl: 'https://github.com/waldo-vision/docs/tree/master',
         },
         blog: {
           showReadingTime: true,
