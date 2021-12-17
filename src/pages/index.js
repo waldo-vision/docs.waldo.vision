4f114a7f25a2f9543a8a7fd7b0a4c6d3064d7dc9
tsundere <benlmartin.work@gmail.com>
2021-12-17 21:49:24 +0000

    Fixing Mobile responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,33 +4,39 @@ import Layout from '@theme/Layout';
 import Head from '@docusaurus/Head';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import styles from './styles.module.css';
+
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout>
       <Head>
+        <meta name="author" content="WALDO Team" />
+        <title>WALDO ⋅ Anticheat powered by AI</title>
+        <meta name="description" content="Cheat detection powered by AI" />
         <meta name="keywords" content="WALDO, Vision, Cheaters, Cheats, Hacks, Anti-Cheat, AI, Machine Learning, Cloud, Open Source, Free, Electron, Gaming, Rainbow 6, CSGO, Valorant, Auto, GTA, FPS, Gamers, Shooters, Mr Homeless, Basically Homeless, Homeless, Youtube, Discord, Docs, Documentation, Minecraft"/>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="language" content="English"/>
-        <meta name="author" content="WALDO Team"/>
-        <title>WALDO ⋅ Anticheat powered by AI</title>
-        <meta name="description" content={siteConfig.tagline} />
         
       </Head>
-    <main>
+
       <header className={styles.header}>
         <div className="container">
           <div className={clsx(styles.header_row, "row")}>
             <div className={clsx(styles.header_row_col__4_col__offset_1, "col col--4 col--offset-1")}>
-              <h1 className={styles.header_row_col__4_col__offset_1__title}>WALDO Documentation</h1>
-              <p className={styles.header_row_col__4_col__offset_1__subtitle}>Welcome to the official WALDO documentation.</p>
+              <h1 className={styles.header_row_col__4_col__offset_1__title}>WALDO</h1>
+              <p className={styles.header_row_col__4_col__offset_1__subtitle}>The Official WALDO Documentation</p>
               <div className={styles.buttons}>
                 <Link
                   className={clsx('button button--outline button--primary button--lg', styles.header_row_col__4_col__offset_1__getStarted,)}
-                  href='./docs'>
-                    Get Started                  
+                  to='/docs'>
+                  Get Started
+                </Link>
+                <Link
+                  className={clsx('button button--outline button--primary button--lg', styles.header_row_col__4_col__offset_1__learnMore,)}
+                  to='/blog'>
+                  Learn More
                 </Link>
               </div>
             </div>
@@ -40,7 +46,7 @@ export default function Home() {
           </div>
         </div>
       </header>
-    </main>
+
     </Layout>
   );
 }
